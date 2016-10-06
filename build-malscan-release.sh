5416e485c4e88d5e4feef24626747ea0c084445f
Josh <josh@joshgrancell.com>
2016-10-06 13:17:47 -0400

    Fixed temp issue

--- build-malscan-release.sh
+++ build-malscan-release.sh
@@ -35,7 +35,7 @@ cp /home/makerpm/rpmbuild/malscan/rpm-build/malscan-el.repo "$SOURCEDIR/malscan.
 ## Packaging the files
 cd "$TEMP"
 wget https://repo.malscan.org/RPM-GPG-KEY-Malscan -P "$SOURCEDIR/"
-tar -czvf "$TEMP/malscan-release-$VERSION.tar.gz" "$SOURCEDIR"
+tar -czvf "malscan-release-$VERSION.tar.gz" "malscan-release-$VERSION"
 
 # Moving the newly packaged files into the build sources directory
 mv "$TEMP/malscan-release-$VERSION.tar.gz" "/home/makerpm/rpmbuild/SOURCES/"
